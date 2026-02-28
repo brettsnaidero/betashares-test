41ef73be43131b0d4acef918c7b7c5b16a9f9d08
Brett Snaidero <brettsnaidero@hipagesgroup.com.au>
2026-02-28 17:19:25 +1100

    chore: add storybook config files

---
+++ .storybook/preview.ts
@@ -0,0 +1,18 @@
+import type { Preview } from "@storybook/nextjs-vite";
+
+import "../src/styles/reset.css";
+import "../src/styles/variables.css";
+import "../src/styles/globals.css";
+
+const preview: Preview = {
+  parameters: {
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+  },
+};
+
+export default preview;
