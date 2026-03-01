bdaca18baff733f6876119b2cd89e85e27ffa32b
Brett Snaidero <brettsnaidero@hipagesgroup.com.au>
2026-03-01 17:58:39 +1100

    fix: remove github pages link and fix base path config

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,10 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: "/betashares-test",
+  basePath: isProd ? "/betashares-test" : "",
   reactStrictMode: true,
   images: {
     unoptimized: true,
