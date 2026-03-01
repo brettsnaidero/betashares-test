5b87bbd161dc063176862e1fc826dc034c09437b
Brett Snaidero <brettsnaidero@hipagesgroup.com.au>
2026-03-01 17:40:08 +1100

    feat: set up static build for github pages

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
+  basePath: "/betashares-test",
   reactStrictMode: true,
   images: {
     unoptimized: true,
